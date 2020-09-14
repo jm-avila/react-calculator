f7e64800db5f44aaa6604ba324dd93743a9c8c9a
jm-avila <jm.avila1309@icloud.com>
2020-09-14 10:51:03 +0200

    Updates

---
+++ precache-manifest.c8223a5d724d9e6e1b507253747b265d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c672a25a81af43100e9bc8e2980efb8",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "317101e76947d9a44385",
+    "url": "/react-calculator/static/css/main.27409565.chunk.css"
+  },
+  {
+    "revision": "fac93bd53f0f0724a44c",
+    "url": "/react-calculator/static/js/2.8f81117f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-calculator/static/js/2.8f81117f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "317101e76947d9a44385",
+    "url": "/react-calculator/static/js/main.cd6351b3.chunk.js"
+  },
+  {
+    "revision": "3c4ee116adfa9d8605e9",
+    "url": "/react-calculator/static/js/runtime-main.5a7a9823.js"
+  }
+]);
